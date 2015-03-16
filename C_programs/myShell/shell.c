5df74bb2c84a4458470c2b05bf7e1d14df38fe24
syfung <joshua.fung@mail.utoronto.ca>
2015-03-16 16:31:01 -0400

    removed finished todo list

--- C_programs/myShell/shell.c
+++ C_programs/myShell/shell.c
@@ -45,6 +45,7 @@ int main(int argc, char** argv) {
     fgets(command_line, MAX_COMMAND, stdin);
     /* Strip the new line character */
     if (command_line[strlen(command_line) - 1] == '\n') {
+
       command_line[strlen(command_line) - 1] = '\0';
     }
 		
@@ -92,35 +93,12 @@ int main(int argc, char** argv) {
  *       absolute path:  cd /u/bogdan/csc209/assignment3/
  */
 int execute_cd(char** words) {
-	
-  /**
-   * TODO: 
-   * The first word contains the "cd" string, the second one contains 
-   * the path.
-   * Check possible errors:
-   * - The words pointer could be NULL, the first string or the second 
-   *   string could be NULL, or the first string is not a cd command
-   * - If so, return an EXIT_FAILURE status to indicate something is 
-   *   wrong.
-   */
+  
   if (words == NULL || words[0] == NULL ||\
       words[1] == NULL || strcmp(words[0],"cd")) {
     printf("EXIT_FAILURE: %d\n",EXIT_FAILURE);
     return EXIT_FAILURE;
   }
-
-  /**
-   * TODO: 
-   * The safest way would be to first determine if the path is relative 
-   * or absolute (see is_relative function provided).
-   * - If it's not relative, then simply change the directory to the path 
-   * specified in the second word in the array.
-   * - If it's relative, then make sure to get the current working 
-   * directory, append the path in the second word to the current working
-   * directory and change the directory to this path.
-   * Hints: see chdir and getcwd man pages.
-   * Return the success/error code obtained when changing the directory.
-   */
 
   if (is_relative(words[1])) {
     printf("Changing directry with relative path\n");
@@ -149,21 +127,6 @@ int execute_cd(char** words) {
  */
 int execute_command(char **tokens) {
 	
-  /**
-   * TODO: execute a program, based on the tokens provided.
-   * The first token is the command name, the rest are the arguments 
-   * for the command. 
-   * Hint: see execlp/execvp man pages.
-   * 
-   * - In case of error, make sure to use "perror" to indicate the name
-   *   of the command that failed.
-   *   You do NOT have to print an identical error message to what would 
-   *   happen in bash.
-   *   If you use perror, an output like: 
-   *      my_silly_command: No such file of directory 
-   *   would suffice.
-   * Function returns only in case of a failure (EXIT_FAILURE).
-   */
 
   /* The exec program should exit itself */
   if (execvp(tokens[0],tokens) == -1) {
@@ -178,20 +141,7 @@ int execute_command(char **tokens) {
  * Executes a non-builtin command.
  */
 int execute_nonbuiltin(simple_command *s) {
-  /**
-   * TODO: Check if the in, out, and err fields are set (not NULL),
-   * and, IN EACH CASE:
-   * - Open a new file descriptor (make sure you have the correct flags,
-   *   and permissions);
-   * - redirect stdin/stdout/stderr to the corresponding file.
-   *   (hint: see dup2 man pages).
-   * - close the newly opened file descriptor in the parent as well. 
-   *   (Avoid leaving the file descriptor open across an exec!) 
-   * - finally, execute the command using the tokens (see execute_command
-   *   function above).
-   * This function returns only if the execution of the program fails.
-   */
-
+  
   printf("Execute_nonbuiltin\n");
 
   /* Redirecting stdin */
@@ -231,17 +181,6 @@ int execute_nonbuiltin(simple_command *s) {
  */
 int execute_simple_command(simple_command *cmd) {
 
-  /**
-   * TODO: 
-   * Check if the command is builtin.
-   * 1. If it is, then handle BUILTIN_CD (see execute_cd function provided) 
-   *    and BUILTIN_EXIT (simply exit with an appropriate exit status).
-   * 2. If it isn't, then you must execute the non-builtin command. 
-   * - Fork a process to execute the nonbuiltin command 
-   *   (see execute_nonbuiltin function above).
-   * - The parent should wait for the child.
-   *   (see wait man pages).
-   */
   /* Check if it is a builtin command */
   switch (cmd->builtin) {
   case BUILTIN_CD:
@@ -282,15 +221,6 @@ int execute_simple_command(simple_command *cmd) {
  * together with a pipe operator.
  */
 int execute_complex_command(command *c) {
-	
-  /**
-   * TODO:
-   * Check if this is a simple command, using the scmd field.
-   * Remember that this will be called recursively, so when you encounter
-   * a simple command you should act accordingly.
-   * Execute nonbuiltin commands only. If it's exit or cd, you should not 
-   * execute these in a piped context, so simply ignore builtin commands. 
-   */
 
   if (c->scmd) {
     printf("Executing nonbuilting in complex command\n");
@@ -304,42 +234,14 @@ int execute_complex_command(command *c) {
    */
 
   if (!strcmp(c->oper, "|")) {
-		
-    /**
-     * TODO: Create a pipe "pfd" that generates a pair of file 
-     * descriptors, to be used for communication between the 
-     * parent and the child. Make sure to check any errors in 
-     * creating the pipe.
-     */
 
     int pfd[2], pid;
     if (pipe(pfd) != 0) {
       perror("pipe execute_complex_command()");
       return -1;
     }
-			
-    /**
-     * TODO: Fork a new process.
-     * In the child:
-     *  - close one end of the pipe pfd and close the stdout 
-     * file descriptor.
-     *  - connect the stdout to the other end of the pipe (the 
-     * one you didn't close).
-     *  - execute complex command cmd1 recursively. 
-     * In the parent: 
-     *  - fork a new process to execute cmd2 recursively.
-     *  - In child 2:
-     *     - close one end of the pipe pfd (the other one than 
-     *       the first child), and close the standard input file 
-     *       descriptor.
-     *     - connect the stdin to the other end of the pipe (the 
-     *       one you didn't close).
-     *     - execute complex command cmd2 recursively. 
-     *  - In the parent:
-     *     - close both ends of the pipe. 
-     *     - wait for both children to finish.
-     */
-    int comStatus;
+    
+    int comStatus, pid2, status2;
 
     if ((pid = fork()) == 0) {
       /* Linking the pipe as stdout */
@@ -352,7 +254,6 @@ int execute_complex_command(command *c) {
       exit(0);
     }
     else if (pid > 0) {
-      int pid2, status2;
       if ((pid2 = fork()) == 0) {
 	/* Linking the pipe as stdin */
 	close(pfd[1]);
@@ -386,6 +287,10 @@ int execute_complex_command(command *c) {
       exit(1);
     }
 		
+  }
+
+  else if (!strcmp(c->oper, ";")) {
+    
   }
   return 0;
 }
