6e8acc2bb71f6e5098a056f43e502bfb6192bfa0
syfung <joshua.fung@mail.utoronto.ca>
2015-03-20 01:00:39 -0400

    problem set 3

--- MATLAB/MSE238/problem_set3.m
+++ MATLAB/MSE238/problem_set3.m
@@ -10,7 +10,7 @@ format compact;
 
 OA = [80,-50,-30,0;
       -50,100,-10,-25;
-      -30,0,65,-20;
+      -30,-10,65,-20;
       0,-25,-20,100];
   
 OB = [-120,0,0,0]';
@@ -76,29 +76,31 @@ disp(x_gs);
 %% MATLAB Eigenvalues Eigenvectors
 [eigenVec, eigenVal] = eig(OA);
 
-fprintf('Eigenvalues\n');
+fprintf('MATLAB Eigenvalues\n');
 disp(eigenVal);
-fprintf('Eigenvectors\n');
+fprintf('MATLAB Eigenvectors\n');
 disp(eigenVec);
 
 %% QR Factorization
 A_qr = OA;
-for i = 1:20
+for i = 1:50
     [q,R] = qr(A_qr);
     A_qr = R * q;
 end
+fprintf('QR factorization\n')
 disp(A_qr)
 
 %% Question 2, Newtoon-Raphson method
 
-%syms t2_t1;
-syms x
 gamma = 5/3;
 eta = @(t2_t1) ((log(t2_t1) - (1-((t2_t1)^-1)))/(log(t2_t1)+(1-((t2_t1)^-1))/(gamma-1)));
 
 etaAt30 = @(t2_t1) eta(t2_t1) - 0.3;
 
-%diffeq = char(diff(etaAt30(x)));
+% % The equation of the dirivitive of eta is found with MATLAB sysmbolic tool
+% % box
+% syms x
+% diffeq = char(diff(etaAt30(x)));
 
 diffEtaAt30 = @(t2_t1) (1/t2_t1 - 1/t2_t1^2)/(log(t2_t1) - 3/(2*t2_t1) + 3/2)...
    - ((1/t2_t1 + 3/(2*t2_t1^2))*(log(t2_t1) + 1/t2_t1 - 1))/(log(t2_t1) - 3/(2*t2_t1) + 3/2)^2;
@@ -109,6 +111,6 @@ for i = 1:20
     t2_t1_ans = t2_t1_ans - ((etaAt30(t2_t1_ans)) / diffEtaAt30(t2_t1_ans));
 end
 
-fprintf('T2/T1 where eta = 0.3:')
+fprintf('T2/T1 where eta = 0.3:');
 disp(t2_t1_ans);
 
