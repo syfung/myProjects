a4241d7f27e8c21fb62812b7956b617e56da8970
syfung <joshua.fung@mail.utoronto.ca>
2015-03-27 04:47:20 -0400

    Sloved the really slow problem, I started not wanting to include math.h. But I gave up, changed the divosor function to search up to sqrt(n)

--- C_programs/projectEuler/Q12/Q12.c
+++ C_programs/projectEuler/Q12/Q12.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #ifndef NUMDIV
-#define NUMDIV 5
+  #define NUMDIV 5
 #endif
 
-long int gen_tri_num (int num);
-int num_div (long int triNum);
+int gen_tri_num (int num);
+int num_div (int triNum);
 
 int main (void) {
-  int i, j, intr = 100;
-  for (i = 1; ; i += intr) {
-    printf("\nstill under\n");
-    if (num_div(gen_tri_num(i)) >= NUMDIV) {
-      printf("over: %ld\n", gen_tri_num(i));
-      intr = (intr / 2);
-      printf("working\n");
-      for (j = i-intr; ; j -= intr) {
-	//printf("working\n");
-	if ((int)gen_tri_num(i) == (int)gen_tri_num(j)) {
-	  printf("ans: %ld\n", gen_tri_num(j));
-	  exit(0);
-	}
-	if (num_div(gen_tri_num(j)) <= NUMDIV) {
-	  printf("under: %ld\n", gen_tri_num(j));
-	  i = j;
-	  break;
-	}
-      }
-    }
+  int i, triNum = 1;
+  for (i = 0;  num_div(triNum) < NUMDIV; i++) {
+    triNum = gen_tri_num(i);
   }
+  printf("ans: %d", triNum);
   return 0;
 }
 
 
-
-long int gen_tri_num (int num) {
-  return (num * (num + 1)) / 2;
+int gen_tri_num (int num) {
+  return ((num * (num + 1)) / 2);
 }
 
-int num_div (long int n) {
-  int counter,i;
-  printf("num %ld", n);
-  for(counter=0,i=1;(!(n%i) && (counter++)) || i<=(n/2);i++);
-  if (n==2 || n==1) {
-      return counter;
+
+int num_div (int n) {
+  int counter = 1,i = 2;
+  
+  while (i < sqrt(n)) {
+
+    if (n % i == 0) {
+      counter++;
+      if (i != n / i) {
+	counter++;
+      }
     }
-  printf("div working numdiv: %d\n", counter);
-  return ++counter;
+
+    i++;
+  }
+
+  if ((i * i) == n) {
+    counter++;
+  }
+
+  return counter;
 }
+
   
